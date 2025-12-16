fadb8aca5a34ad04415cffd68cfb8c9a8415486d
Jesús Ortiz <alu.22130574@correo.itlalaguna.edu.mx>
2025-12-15 23:47:03 -0600

    Agregar archivos de proyecto.

---
+++ Examen/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Examen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            dataGridView1 = new DataGridView();
+            timer1 = new System.Windows.Forms.Timer(components);
+            cmbPuertos = new ComboBox();
+            btnConectar = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(218, 82);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(360, 233);
+            dataGridView1.TabIndex = 0;
+            // 
+            // cmbPuertos
+            // 
+            cmbPuertos.FormattingEnabled = true;
+            cmbPuertos.Location = new Point(373, 40);
+            cmbPuertos.Name = "cmbPuertos";
+            cmbPuertos.Size = new Size(121, 23);
+            cmbPuertos.TabIndex = 1;
+            // 
+            // btnConectar
+            // 
+            btnConectar.Location = new Point(290, 38);
+            btnConectar.Name = "btnConectar";
+            btnConectar.Size = new Size(75, 23);
+            btnConectar.TabIndex = 2;
+            btnConectar.Text = "conectar";
+            btnConectar.UseVisualStyleBackColor = true;
+            btnConectar.Click += btnConectar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnConectar);
+            Controls.Add(cmbPuertos);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timer1;
+        private ComboBox cmbPuertos;
+        private Button btnConectar;
+    }
+}
---
+++ Examen/Form1.cs
@@ -0,0 +1,126 @@
+
+using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+
+namespace Examen
+{
+    public partial class Form1 : Form
+    {
+        SerialPort puertoSerie;
+        string conexion = "Server=DESKTOP-U39V4L7;Database=Registro;Integrated Security=True;TrustServerCertificate=True;";
+
+
+
+
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            CargarRegistros();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1500;
+            timer1.Tick += Timer1_Tick;
+            timer1.Start();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbPuertos.Items.AddRange(SerialPort.GetPortNames());
+            CargarRegistros();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cmbPuertos.SelectedItem == null)
+                {
+                    MessageBox.Show("falta el puerto ");
+                    return;
+                }
+
+                puertoSerie = new SerialPort(cmbPuertos.Text, 9600);
+                puertoSerie.DataReceived += PuertoSerie_DataReceived;
+                puertoSerie.Open();
+
+                //lblEstado.Text = "Conectado a " + cmbPuertos.Text;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar: " + ex.Message);
+            }
+        }
+        private void RegistrarEvento()
+        {
+            DateTime ahora = DateTime.Now;
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(conexion))
+                {
+                    con.Open();
+
+                    SqlCommand cmd = new SqlCommand(
+                        "insert into Registros (Fecha, Hora, Puerto) values (@fecha, @hora, @puerto)", con);
+
+                    cmd.Parameters.AddWithValue("@fecha", ahora.Date);
+                    cmd.Parameters.AddWithValue("@hora", ahora.TimeOfDay);
+                    cmd.Parameters.AddWithValue("@puerto", puertoSerie.PortName);
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                CargarRegistros();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error SQL: " + ex.Message);
+            }
+        }
+        private void PuertoSerie_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string dato = puertoSerie.ReadLine().Trim();
+
+                if (dato == "presiona2")
+                {
+                    Invoke(new Action(RegistrarEvento));
+                }
+            }
+            catch { }
+        }
+
+
+        private void CargarRegistros()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(conexion))
+                {
+                    SqlDataAdapter da = new SqlDataAdapter("select * from Registros order by Id desc", con);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            catch { }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (puertoSerie != null && puertoSerie.IsOpen)
+            {
+                puertoSerie.Close();
+            }
+        }
+    }
+}
